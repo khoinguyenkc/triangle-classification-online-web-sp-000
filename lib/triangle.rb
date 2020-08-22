22954617861136ece06bf26b22d3284e46dbbccc
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-22 04:04:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,12 +21,12 @@ class Triangle
       return
     elsif (@one == @two ) && (@two == @three )
       type = :equilateral
-    elsif (@one != @two ) &&(@two == @three) #exactly 2 3 same
-      type = :isoceles
-    elsif (@one != @two ) &&(@one == @three) #eactly 1 3 same
-      type = :isoceles
+    elsif (@one != @two ) && (@two == @three) #exactly 2 3 same
+      type = :isosceles
+    elsif (@one != @two ) && (@one == @three) #eactly 1 3 same
+      type = :isosceles
     elsif (@three != @two ) && (@one == @two) #eactly 1 2 same
-      type = :isoceles
+      type = :isosceles
     elsif (@one != @two) && (@two != @three) && (@one != @three) #all 3 sides dif but length valid
       type = :scalene
     end
