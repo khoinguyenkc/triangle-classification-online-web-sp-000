229e72728c0eedc738457eeafb1148c92b58ccb5
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-22 03:41:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
+  def initialize(one, two, three)
+    @one = one,
+    @two = two,
+    @three = three
+  end
+
+  def kind
+    valid = true
+    type = ""
+
+
+    if (@one + @two < @three) || (@one + @three < @two) || (@two + @three < @one)
+      raise TriangleError
+      return
+    elsif (@one == @two ) && (@two == @three )
+      type = "equilateral"
+    elsif (@one != @two ) &&(@two == @three)
+    elsif 
+    elsif condition
+      
+      
+      
+    end
+
+
+    if valid 
+      return type
+        end
+      
+  end
+
+  class TriangleError < StandardError
+    def 
+  end 
+  
 end
